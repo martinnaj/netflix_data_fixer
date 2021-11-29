703834fe047ee4f3ac6008cb1996eb352430c8a6
martinnaj <martin@najemi.cz>
2021-11-29 11:28:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This program will cleanup any Netflix exported data
-# import csv
 from inspect import getfullargspec
 from os.path import exists
+from re import split
 
 
 class Modes:
@@ -9,60 +9,20 @@ class Modes:
 
     def __init__(self):
         self.data = [x for x in dir(self) if not x.startswith('__') and not x.endswith('__')]
-    
 
-    def my_list___remove_profile(self, file_name):
-        file_lines = open(file_name, 'r').read().split('\n')
+    def __remove_column__(self, file_name):
+        file_lines = open(file_name, 'r', encoding="utf-8").read().split('\n')
 
-        first_line = file_lines.pop(0)
-
-        data = {j: [] for j in first_line.split(',')}
-
-        self.__finish__() if 'Profile Name' not in data.keys() else None
-
-        profiles = list({j.split(',')[list(data.keys()).index('Profile Name')]: 0 for j in file_lines if
-                         j.split(',')[list(data.keys()).index('Profile Name')]}.keys())
-
-        another_profile = len(profiles) > 1
-        while another_profile:
-            print("Please select one of the following profiles to remove:")
-            print(', '.join(profiles).title())
-
-            chosen_profile = input()
-            while not len([j for j in [p.lower() for p in profiles] if j.startswith(chosen_profile)]) == 1:
-                print("Profile not found")
-                chosen_profile = input().lower()
-
-            chosen_profile = profiles[[p.lower() for p in profiles].index(
-                [j for j in [p.lower() for p in profiles] if j.startswith(chosen_profile)][0])]
-
-            profiles.pop(profiles.index(chosen_profile))
-
-            file_lines = [j for j in file_lines if not j.split(',')[0] == chosen_profile]
-
-            print("Would you like to remove another profile? (y/n)")
-
-            file = open(file_name, 'w+')
-            file.write(first_line + '\n' + '\n'.join(file_lines))
-            file.close()
-
-            another_profile = input().lower() == 'y' and len(profiles)
-
-        self.__finish__()
-
-    def my_list___remove_column(self, file_name):
-        file_lines = open(file_name, 'r').read().split('\n')
-
-        columns = file_lines.pop(0).split(',')
+        columns = split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', file_lines.pop(0))
 
         another_column = len(columns) > 1
         while another_column:
             print("Please select one of the following columns to remove:")
             print(', '.join(columns).title())
 
-            chosen_column = input()
+            chosen_column = input().lower()
             while not len([j for j in [p.lower() for p in columns] if j.startswith(chosen_column)]) == 1:
-                print("Column not found")
+                print("Column not found or matches multiple")
                 chosen_column = input().lower()
 
             chosen_column = columns[[p.lower() for p in columns].index(
@@ -70,22 +30,183 @@ class Modes:
 
             column_index = columns.index(chosen_column)
 
-            columns.pop(columns.index(chosen_column))
+            columns.pop(column_index)
 
-            file_lines = [','.join([k for k in j.split(',') if not j.split(',').index(k) == column_index]) for j in
-                          file_lines]
+            file_lines = [','.join([split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j)[k] for k in
+                                    range(len(split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j))) if
+                                    not k == column_index]) for j in file_lines]
 
             print("Would you like to remove another column? (y/n)")
 
-            file = open(file_name, 'w+')
+            file = open(file_name, 'w+', encoding="utf-8")
             file.write(','.join(columns) + '\n' + '\n'.join(file_lines))
             file.close()
 
             another_column = input().lower() == 'y' and len(columns)
 
+    def __only_one_profile__(self, file_name, profile_column_name):
+        file_lines = open(file_name, 'r', encoding="utf-8").read().split('\n')
+
+        first_line = file_lines.pop(0)
+
+        data = {j: [] for j in split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', first_line)}
+
+        self.__finish__() if profile_column_name not in data.keys() else None
+
+        profiles = list(
+            {split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j)[list(data.keys()).index(profile_column_name)]: 0 for j in
+             file_lines if
+             split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j)[list(data.keys()).index(profile_column_name)]}.keys())
+
+        print("Please select one of the following profiles to keep:")
+        print(', '.join(profiles).title())
+
+        chosen_profile = input().lower()
+        while not len([j for j in [p.lower() for p in profiles] if j.startswith(chosen_profile)]) == 1:
+            print("Profile not found or matches multiple")
+            chosen_profile = input().lower()
+
+        chosen_profile = profiles[[p.lower() for p in profiles].index(
+            [j for j in [p.lower() for p in profiles] if j.startswith(chosen_profile)][0])]
+
+        file_lines = [j for j in file_lines if
+                      split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j)[
+                          list(data.keys()).index(profile_column_name)] == chosen_profile]
+
+        file = open(file_name, 'w+', encoding="utf-8")
+        file.write(first_line + '\n' + '\n'.join(file_lines))
+        file.close()
+
+    def __remove_profile__(self, file_name, profile_column_name):
+        file_lines = open(file_name, 'r', encoding="utf-8").read().split('\n')
+
+        first_line = file_lines.pop(0)
+
+        data = {j: [] for j in split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', first_line)}
+
+        self.__finish__() if profile_column_name not in data.keys() else None
+
+        profiles = list(
+            {split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j)[list(data.keys()).index(profile_column_name)]: 0 for j in
+             file_lines if
+             split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j)[list(data.keys()).index(profile_column_name)]}.keys())
+
+        another_profile = len(profiles) > 1
+        while another_profile:
+            print("Please select one of the following profiles to remove:")
+            print(', '.join(profiles).title())
+
+            chosen_profile = input().lower()
+            while not len([j for j in [p.lower() for p in profiles] if j.startswith(chosen_profile)]) == 1:
+                print("Profile not found or matches multiple")
+                chosen_profile = input().lower()
+
+            chosen_profile = profiles[[p.lower() for p in profiles].index(
+                [j for j in [p.lower() for p in profiles] if j.startswith(chosen_profile)][0])]
+
+            profiles.pop(profiles.index(chosen_profile))
+
+            file_lines = [j for j in file_lines if not split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j)[
+                                                           list(data.keys()).index(
+                                                               profile_column_name)] == chosen_profile]
+
+            print("Would you like to remove another profile? (y/n)")
+
+            file = open(file_name, 'w+', encoding="utf-8")
+            file.write(first_line + '\n' + '\n'.join(file_lines))
+            file.close()
+
+            another_profile = input().lower() == 'y' and len(profiles)
+
+    def __remove_by_value__(self, file_name, col_name, value='', remove_all=False):
+        file_lines = open(file_name, 'r', encoding="utf-8").read().split('\n')
+
+        first_line = file_lines.pop(0)
+
+        data = {j: [] for j in split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', first_line)}
+
+        self.__finish__() if col_name not in data.keys() else None
+
+        column_values = list(
+            {split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j)[list(data.keys()).index(col_name)]: 0 for j in file_lines if
+             split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j)[list(data.keys()).index(col_name)]}.keys())
+
+        another_value = len(column_values) > 1
+        while another_value:
+            print("Please select one of the following values to remove:") if not value and not remove_all else None
+            print(', '.join(column_values).title()) if not value and not remove_all else None
+
+            chosen_value = input().lower() if not value and not remove_all else value
+            while not remove_all and not len(
+                    [j for j in [p.lower() for p in column_values] if j.startswith(chosen_value)]) == 1:
+                print("Value not found or matches multiple")
+                chosen_value = input().lower()
+
+            chosen_value = column_values[[p.lower() for p in column_values].index(
+                [j for j in [p.lower() for p in column_values] if j.startswith(chosen_value)][
+                    0])] if not remove_all else column_values[0]
+
+            column_values.pop(column_values.index(chosen_value))
+
+            file_lines = [j for j in file_lines if not split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j)[
+                                                           list(data.keys()).index(col_name)] == chosen_value or (
+                                      not split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', j)[
+                                          list(data.keys()).index(col_name)] and remove_all)]
+
+            print("Would you like to remove another value? (y/n)") if not value and not remove_all else None
+
+            file = open(file_name, 'w+', encoding="utf-8")
+            file.write(first_line + '\n' + '\n'.join(file_lines))
+            file.close()
+
+            another_value = len(column_values) and (remove_all or (input().lower() == 'y' and not value))
+
+    def viewing_activity___remove_trailers_and_hooks(self, file_name):
+        self.__remove_by_value__(file_name, 'Supplemental Video Type', '', True)
+        self.__finish__()
+
+    def viewing_activity___remove_not_latest_bookmark(self, file_name):
+        self.__remove_by_value__(file_name, 'Latest Bookmark', 'not latest view')
+        self.__finish__()
+
+    def viewing_activity___remove_device(self, file_name):
+        self.__remove_by_value__(file_name, 'Device Type')
+        self.__finish__()
+
+    def viewing_activity___remove_profile(self, file_name):
+        self.__remove_profile__(file_name, 'Profile Name')
+        self.__finish__()
+
+    def viewing_activity___only_one_profile(self, file_name):
+        self.__only_one_profile__(file_name, 'Profile Name')
+        self.__finish__()
+
+    def viewing_activity___remove_column(self, file_name):
+        self.__remove_column__(file_name)
+        self.__finish__()
+
+    def search_history___remove_profile(self, file_name):
+        self.__remove_profile__(file_name, 'Profile Name')
+        self.__finish__()
+
+    def search_history___only_one_profile(self, file_name):
+        self.__only_one_profile__(file_name, 'Profile Name')
+        self.__finish__()
+
+    def search_history___remove_column(self, file_name):
+        self.__remove_column__(file_name)
+        self.__finish__()
+
+    def my_list___remove_profile(self, file_name):
+        self.__remove_profile__(file_name, 'Profile Name')
         self.__finish__()
 
     def my_list___only_one_profile(self, file_name):
+        self.__only_one_profile__(file_name, 'Profile Name')
+        self.__finish__()
+
+    def my_list___remove_column(self, file_name):
+        self.__remove_column__(file_name)
         self.__finish__()
 
     def __finish__(self):
@@ -95,7 +216,7 @@ class Modes:
 
 modes = Modes()
 
-chosen_data_store = 'my_list'
+chosen_data_store = 'viewing_activity'
 chosen_data_store = ''
 while not [x for x in modes.data if x.startswith(chosen_data_store + '___')]:
     print("Choose a data type to edit from the following: ")
@@ -116,7 +237,7 @@ argument_values = []
 
 for i in argument_names:
     print("What is the '" + i.replace('_', ' ') + '\'?')
-    input_ = 'MyList.csv'
+    input_ = 'ViewingActivity.csv'
     input_ = input()
     if i == 'file_name':
         while not exists(input_):
